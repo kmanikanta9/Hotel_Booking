0ec98bac02f378a680edc0330bd364e3d7d7461f
manikantak9 <manimanikanta41150@gmail.com>
2025-09-25 19:13:28 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,25 @@ import cors from 'cors'
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
+
 connectDB()
 
 const app = express()
-app.use(cors())  // Enable cross-origin resource sharing
+app.use(cors())
+
 // Middleware
 app.use(express.json())
 
 app.use(clerkMiddleware())
 
-app.use('/api/clerk',clerkWebhooks)
+// Clerk webhook needs raw body, so we use express.raw()
+app.post(
+  '/api/clerk',
+  express.raw({ type: 'application/json' }), 
+  clerkWebhooks
+)
 
-app.get('/',(req,res)=>res.send('API is working.'))
-
+app.get('/', (req, res) => res.send('API is working.'))
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>console.log("Server running on port",PORT))
+app.listen(PORT, () => console.log("Server running on port", PORT))
